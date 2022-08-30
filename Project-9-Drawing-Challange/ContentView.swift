21f2e45316c658f46780d4b46f70afdc9f08fdb9
Luca <luca@weve-got-it.com>
2022-08-30 17:45:24 +0200

    Added challenge 3: challenge 3 - Create a ColorCyclingRectangle shape that is the rectangular cousin of ColorCyclingCircle, allowing us to control the position of the gradient using one or more properties. Commented out challenge 1 and 2 code for challenge 3 to be accessible.

--- Project-9-Drawing-Challange/ContentView.swift
+++ Project-9-Drawing-Challange/ContentView.swift
@@ -13,57 +13,143 @@
 //  properties.
 
 import SwiftUI
+// Project 9 - Challange 1 & 2
+//// Project 9 - Challange 1
+//struct Arrow: Shape {
+//    var headHeight = 0.5
+//    var shaftWidth = 0.3
+//
+//    // Project 9 - Challange 2
+//    var animatableData: AnimatablePair<Double, Double> {
+//        get { AnimatablePair(headHeight, shaftWidth) }
+//        set {
+//            headHeight = newValue.first
+//            shaftWidth = newValue.second
+//
+//        }
+//    }
+//
+//    func path(in rect: CGRect) -> Path {
+//        let height = rect.height * headHeight
+//        let thickness = rect.width * shaftWidth / 2
+//
+//        return Path { path in
+//            path.move(to: CGPoint(x: 0, y: height))
+//            path.addLine(to: CGPoint(x: rect.midX, y: 0))
+//            path.addLine(to: CGPoint(x: rect.maxX, y: height))
+//            path.addLine(to: CGPoint(x: rect.midX + thickness, y: height))
+//            path.addLine(to: CGPoint(x: rect.midX + thickness, y: rect.maxY))
+//            path.addLine(to: CGPoint(x: rect.midX - thickness, y: rect.maxY))
+//            path.addLine(to: CGPoint(x: rect.midX - thickness, y: height))
+//
+//            path.closeSubpath()
+//
+//        }
+//    }
+//}
+//
+//
+//    struct ContentView: View {
+//        // Project 9 - Challange 2
+//        @State private var headHeight = 0.5
+//        @State private var shaftWidth = 0.25
+//
+//        var body: some View {
+//            // Project 9 - Challange 2
+//            Arrow(headHeight: headHeight, shaftWidth: shaftWidth)
+//                .fill(.indigo)
+//                .onTapGesture {
+//                    withAnimation {
+//                        headHeight = Double.random(in: 0.2...0.8)
+//                        shaftWidth = Double.random(in: 0.2...0.8)
+//                    }
+//                }
+//        }
+//}
 
-// Project 9 - Challange 1
-struct Arrow: Shape {
-    var headHeight = 0.5
-    var shaftWidth = 0.3
+// Project 9 - Challange 3
+struct ColourCyclingRectangle: View {
+    var amount = 0.0
+    var steps = 100
     
-    // Project 9 - Challange 2
-    var animatableData: AnimatablePair<Double, Double> {
-        get { AnimatablePair(headHeight, shaftWidth) }
-        set {
-            headHeight = newValue.first
-            shaftWidth = newValue.second
-            
+    var gradientStartX = 0.5
+    var gradientStartY = 0.0
+    var gradientEndX = 0.5
+    var gradientEndY = 1.0
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5),
+                            ]),
+                            startPoint: UnitPoint(x: gradientStartX, y: gradientStartY),
+                            endPoint: UnitPoint(x: gradientEndX, y: gradientEndY)
+                        ),
+                        lineWidth: 2
+                        )
+            }
         }
+        // This modifier greatly improves the performance of the render.
+        // try remove it and see the difference
+        .drawingGroup()
     }
     
-    func path(in rect: CGRect) -> Path {
-        let height = rect.height * headHeight
-        let thickness = rect.width * shaftWidth / 2
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
         
-        return Path { path in
-            path.move(to: CGPoint(x: 0, y: height))
-            path.addLine(to: CGPoint(x: rect.midX, y: 0))
-            path.addLine(to: CGPoint(x: rect.maxX, y: height))
-            path.addLine(to: CGPoint(x: rect.midX + thickness, y: height))
-            path.addLine(to: CGPoint(x: rect.midX + thickness, y: rect.maxY))
-            path.addLine(to: CGPoint(x: rect.midX - thickness, y: rect.maxY))
-            path.addLine(to: CGPoint(x: rect.midX - thickness, y: height))
-            
-            path.closeSubpath()
-            
+        if targetHue > 1 {
+            targetHue -= 1
         }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
-    struct ContentView: View {
-        // Project 9 - Challange 2
-        @State private var headHeight = 0.5
-        @State private var shaftWidth = 0.25
-        
-        var body: some View {
-            // Project 9 - Challange 2
-            Arrow(headHeight: headHeight, shaftWidth: shaftWidth)
-                .fill(.indigo)
-                .onTapGesture {
-                    withAnimation {
-                        headHeight = Double.random(in: 0.2...0.8)
-                        shaftWidth = Double.random(in: 0.2...0.8)
-                    }
-                }
+struct ContentView: View {
+    @State private var colorCycle = 0.0
+    
+    @State private var gradientStartX = 0.5
+    @State private var gradientStartY = 0.0
+    @State private var gradientEndX = 0.5
+    @State private var gradientEndY = 1.0
+    
+    var body: some View {
+        VStack {
+            ColourCyclingRectangle(amount: colorCycle, gradientStartX: gradientStartX, gradientStartY: gradientStartY, gradientEndX: gradientEndX, gradientEndY: gradientEndY)
+                .frame(width: 300, height: 300)
+            
+            HStack {
+                Text("Color")
+                Slider(value: $colorCycle)
+            }
+            
+            HStack {
+                Text("Start X")
+                Slider(value: $gradientStartX)
+            }
+            
+            HStack {
+                Text("Start Y")
+                Slider(value: $gradientStartY)
+            }
+            
+            HStack {
+                Text("End X")
+                Slider(value: $gradientEndX)
+            }
+            
+            HStack {
+                Text("End Y")
+                Slider(value: $gradientEndY)
+            }
         }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
