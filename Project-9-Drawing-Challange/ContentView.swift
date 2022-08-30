6154d1dc2aec66582cbf12b48f074dd71adb496f
Luca <luca@weve-got-it.com>
2022-08-30 16:35:04 +0200

    Initial Commit

---
+++ Project-9-Drawing-Challange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-9-Drawing-Challange
+//
+//  Created by Luca Capriati on 2022/08/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
