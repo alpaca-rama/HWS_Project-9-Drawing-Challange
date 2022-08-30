10d148b9b5bf0e564b5daa58bc2476f1a68fec98
Luca <luca@weve-got-it.com>
2022-08-30 16:50:46 +0200

    Added Challenge Info: Added the three challenges for this project as comments.

--- Project-9-Drawing-Challange/ContentView.swift
+++ Project-9-Drawing-Challange/ContentView.swift
@@ -4,6 +4,13 @@
 //
 //  Created by Luca Capriati on 2022/08/30.
 //
+// Challange 1 - Create an Arrow shape – having it point straight up is fine. This could be a
+//  rectangle/triangle-style arrow, or perhaps three lines, or maybe something else depending on
+//  what kind of arrow you want to draw.
+// Challange 2 - Make the line thickness of your Arrow shape animatable.
+// Challange 3 - Create a ColorCyclingRectangle shape that is the rectangular cousin of
+//  ColorCyclingCircle, allowing us to control the position of the gradient using one or more
+//  properties.
 
 import SwiftUI
 
